70ec51bb418c52a9aee743f518c3943d260c319d
oliverbenjamin.hostettler <oliverbenjamin.hostettler@uzh.ch>
2025-01-10 10:35:13 +0100

    Square Movement Behaviour

--- Assets/Scripts/SquareMovement.cs
+++ Assets/Scripts/SquareMovement.cs
@@ -1,16 +1,45 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class SquareMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private float _moveSpeed;
+    private InputAction _moveAction;
+
+    private bool _moving = false;
+    Vector2 moveAxis = new Vector2();
+
+    private void Start()
     {
-        
+        _moveAction = InputSystem.actions.FindAction("Move");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        MoveSquare();
+    }
+
+    private void MoveSquare()
+    {
+
+        if (_moveAction.WasPressedThisFrame())
+        {
+            _moving = true;
+            moveAxis = _moveAction.ReadValue<Vector2>();
+        }
+
+
+
+        if (_moving )
+        {
+            this.transform.Translate(moveAxis * _moveSpeed * Time.deltaTime);
+        }
+    }
+
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        _moving = false;
     }
 }
